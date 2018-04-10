3c235c05be4ae3a3903de4e3716770949d441f8b
Eric Stavarache <ericptst@gmail.com>
2018-04-10 13:40:40 +0300

    Improve download_dataset.sh

--- download_dataset.sh
+++ download_dataset.sh
@@ -3,8 +3,32 @@
 
 FILE=$1
 
-if [[ $FILE != "ae_photos" && $FILE != "apple2orange" && $FILE != "summer2winter_yosemite" &&  $FILE != "horse2zebra" && $FILE != "monet2photo" && $FILE != "cezanne2photo" && $FILE != "ukiyoe2photo" && $FILE != "vangogh2photo" && $FILE != "maps" && $FILE != "cityscapes" && $FILE != "facades" && $FILE != "iphone2dslr_flower" && $FILE != "ae_photos" ]]; then
-    echo "Available datasets are: apple2orange, summer2winter_yosemite, horse2zebra, monet2photo, cezanne2photo, ukiyoe2photo, vangogh2photo, maps, cityscapes, facades, iphone2dslr_flower, ae_photos"
+DATASETS=(\
+    apple2orange\
+    summer2winter_yosemite\
+    horse2zebra\
+    monet2photo\
+    cezanne2photo\
+    ukiyoe2photo\
+    vangogh2photo\
+    maps\
+    cityscapes\
+    facades\
+    iphone2dslr_flower\
+    ae_photos\
+    )
+
+
+found=false
+
+for d in ${DATASETS[@]}; do
+    if [[ "$d" == "$FILE" ]]; then
+        found=true
+    fi
+done
+
+if ! $found; then
+    echo "Available datasets: ${DATASETS[@]}"
     exit 1
 fi
 
